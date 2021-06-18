b568c6369c38ceb7ac8c54849929dba8dea03588
TomasGlgg <globistomas@gmail.com>
2021-06-18 13:34:27 +0300

    Fix visualiser (лучше так)

--- visualiser.py
+++ visualiser.py
@@ -1,16 +1,15 @@
 import matplotlib.pyplot as plt
 
-while True:
-    with open("result.txt") as f:
-        data = f.readlines()
+with open("result.txt") as f:
+    data = f.readlines()
 
-    points_x = list()
-    points_y = list()
-    data.sort()
-    for line in data:
-        x, y = line.rstrip().split(', ')
-        points_x.append(float(x[3:]))
-        points_y.append(float(y[3:]))
+points_x = list()
+points_y = list()
+data.sort()
+for line in data:
+    x, y = line.rstrip().split(', ')
+    points_x.append(float(x[3:]))
+    points_y.append(float(y[3:]))
 
-    plt.plot(points_x, points_y)
-    plt.show()
+plt.plot(points_x, points_y)
+plt.show()
